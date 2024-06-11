6451abf325b039fb9f00febb680c3c22794232ff
williamding123 <williamdingwoodruff@gmail.com>
2024-06-11 10:52:39 -0400

    fix: add items -> please add items

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -106,7 +106,7 @@ function AddItemForm({ onNewItem }) {
                         disabled={!newItem.length}
                         className={submitting ? 'disabled' : ''}
                     >
-                        {submitting ? 'Adding...' : 'Add Items'}
+                        {submitting ? 'Adding...' : 'Please add items'}
                     </Button>
                 </InputGroup.Append>
             </InputGroup>
